67ada3b87d84dd39b4da07c91c197843b53edfa1
Robert Andrade <robert_andrade@icloud.com>
2020-11-20 16:21:54 -0300

    Backend Olx B7W

---
+++ src/validators/AuthValidator.js
@@ -0,0 +1,29 @@
+const { checkSchema } = require ('express-validator');
+
+module.exports = {
+    signup: checkSchema ({
+        name: {
+            trim: true,
+            notEmpty: true,
+            isLength:{
+                options: { min: 2}
+            },
+            errorMessage: 'Nome precisa ter pelo menos 2 caracteres'
+        },
+        email: {
+            isEmail: true,
+            normalizeEmail: true,
+            errorMessage: 'E-mail inválido'
+        },
+        password: {
+            isLength: {
+                options: { min: 4}
+            },
+            errorMessage: 'Senha precisa ter pelo menos 4 caracteres'
+        },
+        state: {
+            notEmpty: true,
+            errorMessage: 'Favor selecione o estado'
+        }
+    })
+};
